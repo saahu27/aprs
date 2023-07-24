63074ebb30c310b63788dd5d4f3482715d8aa346
saahu27 <sahrudaypatti@gmail.com>
2023-07-24 14:27:50 -0400

    description files edited

---
+++ aprs_ws/build/franka_moveit_config/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/workspaces/aprs/aprs_ws/install/franka_description/share/franka_description/package.sh"
---
+++ aprs_ws/build/panda_moveit_config/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/workspaces/aprs/aprs_ws/install/panda_description/share/panda_description/package.sh"
