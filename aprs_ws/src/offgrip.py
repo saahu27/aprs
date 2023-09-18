d52f2f08a6794f8fb1f28b4524cf822ee1106e47
saahu27 <sahrudaypatti@gmail.com>
2023-09-18 15:40:17 -0400

    gripper control, cccconveyor control

--- aprs_ws/src/offgrip.py
+++ aprs_ws/src/offgrip.py
@@ -7,22 +7,16 @@ SEC_PORT = 54321  # The port used by the server
 
 i_sec = socket.create_connection((HOST, SEC_PORT), timeout=0.5)
 
-hex_payload = bytes.fromhex("09 10 03 E8 00 03 06 00 00 00 00 00 00 73 30")
-i_sec.send(hex_payload)
-
 hex_payload = bytes.fromhex("09 10 03 E8 00 03 06 01 00 00 00 00 00 72 E1")
 i_sec.send(hex_payload)
 
-hex_payload = bytes.fromhex("09 03 07 D0 00 01 85 CF")
-i_sec.send(hex_payload)
-
 sleep(0.3)
-
-hex_payload = bytes.fromhex("09 10 03 E8 00 03 06 09 00 00 FF FF FF 42 29")
+hex_payload = bytes.fromhex("09 10 03 E8 00 03 06 09 00 00 00 FF FF 72 19")
 i_sec.send(hex_payload)
 
 chunk = i_sec.recv(2048)
 print(chunk)
+
 #with open("gripper_close.txt","r") as f:
 #    close_program = f.read()
 #    i_sec.send(str.encode(close_program))
